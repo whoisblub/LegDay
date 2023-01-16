6ca996cf48cfec2af2ce40c3d8600c7f3d316d1b
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 17:28:20 -0500

    initial commit

---
+++ LegDay/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BepInEx;
+using EFT;
+using Comfort.Common;
+using BepInEx.Logging;
+using Aki.Reflection.Patching;
+using System.Reflection;
+
+namespace LegDay
+{
+    [BepInPlugin("com.blub.legday", "LegDay", "1.0.0")]
+    internal class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = base.Logger;
+
+            Prefs.Bind(Config);
+            
+            Log.LogInfo("tren has been injected");
+        }
+
+        private void Start()
+        {
+            new GameWorldStartedPatch().Enable();
+        }
+    }
+
+    internal class GameWorldStartedPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(GameWorld).GetMethod("OnGameStarted", BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        [PatchPostfix]
+        static void GameWorld_OnGameStarted_PostFix()
+        {
+            Prefs.FindBuffReferences(Singleton<GameWorld>.Instance.AllPlayers[0].Skills);
+            Plugin.Log.LogInfo("Found all Player Skill Buffs");
+        }
+    }
+}
---
+++ LegDay/Prefs.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx.Configuration;
+using System.Reflection;
+
+namespace LegDay
+{
+    internal static class Prefs
+    {
+        public static Buff jumpBuff;
+        public static Buff sprintBuff;
+
+        public static void Bind(ConfigFile config)
+        {
+            jumpBuff = new Buff(config.Bind("LegDay", "JumpBuff", 0f, "Change Jump Strength Buff"), "StrengthBuffJumpHeightInc");
+            sprintBuff = new Buff(config.Bind("LegDay", "SprintBuff", 0f, "Change Sprint Speed Buff"), "StrengthBuffSprintSpeedInc");
+        }
+
+        public static void FindBuffReferences(SkillsClass skills)
+        {
+            // @Todo: put all buffs in an array so we can just loop over them
+            jumpBuff.FindReference(skills);
+            sprintBuff.FindReference(skills);
+        }
+
+        public class Buff
+        {
+            public ConfigEntry<float> entry;
+            public SkillsClass.GClass1563 reference;
+
+            private string referenceName;
+
+            public Buff(ConfigEntry<float> entry, string referenceName)
+            {
+                this.entry = entry;
+                this.referenceName = referenceName;
+
+                entry.SettingChanged += Entry_SettingChanged;
+            }
+
+            private void Entry_SettingChanged(object sender, System.EventArgs e)
+            {
+                if(reference == null)
+                {
+                    Plugin.Log.LogWarning($"Buff reference ({referenceName}) not found! The mod only works in raid.");
+                    return;
+                }
+
+                reference.Value = entry.Value;
+            }
+
+            public void FindReference(SkillsClass skills)
+            {
+                var info = typeof(SkillsClass).GetField(referenceName, BindingFlags.Public | BindingFlags.Instance);
+                reference = (SkillsClass.GClass1563)info.GetValue(skills);
+            }
+        }
+    }
+}
